12066ff2768d48443fc690427e96bc678a92656d
Luke Clifton <ltclifton@gmail.com>
2014-04-14 10:20:49 +0800

    Updated cabal dependencies and made change for new scrypt API.

--- src/Scat.hs
+++ src/Scat.hs
@@ -20,7 +20,7 @@ import Scat.Options
 -- | Generates the seed integer given a service, a password and a code.
 scatter :: ByteString -> ByteString -> ByteString -> Integer
 scatter k pw c = foldr (\ n s -> fromIntegral n + 256 * s) 0 $
-    unpack $ unHash $ scrypt params (Salt k) (Pass $ pw <> c)
+    unpack $ getHash $ scrypt params (Salt k) (Pass $ pw <> c)
   where
     Just params = scryptParams 14 8 50
 
